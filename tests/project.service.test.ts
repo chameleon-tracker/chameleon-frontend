9f118568061e24ef9fbdfb2a986cfb1294ee9ab9
brt <dubudabu@gmail.com>
2024-02-04 22:02:00 +0100

    Add test: fetch all projects

---
+++ tests/project.service.test.ts
@@ -0,0 +1,41 @@
+import { describe, test, expect, vi, Mock } from 'vitest'
+import { fetchProjects } from '../src/api/project.service'
+import { Project } from '../src/api/types'
+import { ref } from 'vue'
+import { Ref } from 'vue'
+
+
+const fetch = global.fetch = vi.fn() as Mock
+
+
+const createFetchResponse = (data: Ref<Project[]>) => {
+  return { json: () => new Promise((resolve) => resolve(data)) }
+}
+
+describe('Project Service', () => {
+  test('makes a GET request to fetch projects list', async () => {
+    const projectsListResponse = ref<Project[]>([
+      {
+        id: '1',
+        title: 'Project 1 Title',
+        description: 'Project 1 Description',
+        description_markup: 'PLAIN',
+      },
+      {
+        id: '2',
+        title: 'Project 2 Title',
+        description: 'Project 2 Description',
+        description_markup: 'PLAIN',
+      },
+    ])
+
+    const projects = ref<Project[]>([])
+
+    fetch.mockResolvedValue(createFetchResponse(projectsListResponse))
+    const projectsList = await fetchProjects(projects)
+
+    expect(fetch).toHaveBeenCalledWith('/api/project')
+    expect(projectsListResponse).toStrictEqual(projectsList)
+  })
+  test.todo('makes a POST request to create a new project')
+})
